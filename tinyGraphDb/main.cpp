6990c4bb75fb5d4ae74bcc514fa4cda3c539b6a9
Guillaume Collet <guillaume@gcollet.fr>
2012-12-12 16:45:44 +0100

    Initial Commit

---
+++ tinyGraphDb/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  tinyGraphDb
+//
+//  Created by Guillaume Collet on 12/12/12.
+//  Copyright (c) 2012 bilouweb. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
+
